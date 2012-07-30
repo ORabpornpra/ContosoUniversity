ccca3b434faa3e9ea0e093f8e57c85d465c97cdf
orabpornpra <orabpornpra@blendtec.com>
2012-07-30 15:10:18 -0600

    Finish Tutorial 5

---
+++ ContosoUniversity/Controllers/CourseController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Controllers
+{ 
+    public class CourseController : Controller
+    {
+        private SchoolContext db = new SchoolContext();
+
+        //
+        // GET: /Course/
+
+        public ViewResult Index()
+        {
+            //see the .Include key word that mean it's using Eager Loading
+            var courses = db.Courses.Include(c => c.Department);
+            return View(courses.ToList());
+        }
+
+        //
+        // GET: /Course/Details/5
+
+        public ViewResult Details(int id)
+        {
+            Course course = db.Courses.Find(id);
+            return View(course);
+        }
+
+        //
+        // GET: /Course/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.DepartmentID = new SelectList(db.Departments, "DepartmentID", "Name");
+            return View();
+        } 
+
+        //
+        // POST: /Course/Create
+
+        [HttpPost]
+        public ActionResult Create(Course course)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Courses.Add(course);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            ViewBag.DepartmentID = new SelectList(db.Departments, "DepartmentID", "Name", course.DepartmentID);
+            return View(course);
+        }
+        
+        //
+        // GET: /Course/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            Course course = db.Courses.Find(id);
+            ViewBag.DepartmentID = new SelectList(db.Departments, "DepartmentID", "Name", course.DepartmentID);
+            return View(course);
+        }
+
+        //
+        // POST: /Course/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Course course)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(course).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.DepartmentID = new SelectList(db.Departments, "DepartmentID", "Name", course.DepartmentID);
+            return View(course);
+        }
+
+        //
+        // GET: /Course/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            Course course = db.Courses.Find(id);
+            return View(course);
+        }
+
+        //
+        // POST: /Course/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {            
+            Course course = db.Courses.Find(id);
+            db.Courses.Remove(course);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ContosoUniversity/Controllers/InstructorController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ContosoUniversity.Models;
+using ContosoUniversity.ViewModels; //in order to access in the InsructionIndexData in the ViewModels
+
+namespace ContosoUniversity.Controllers
+{ 
+    public class InstructorController : Controller
+    {
+        private SchoolContext db = new SchoolContext();
+
+        //
+        // GET: /Instructor/
+
+        // the .Include key word mean that is Eager Loading
+        //public ViewResult Index()
+        //{
+        //    var instructors = db.Instructors.Include(i => i.OfficeAssignment);
+        //    return View(instructors.ToList());
+        //}
+
+        public ActionResult Index(Int32? id, Int32? courseID)
+        {
+            var viewModel = new InstructorIndexData();
+            viewModel.Instructors = db.Instructors
+                .Include(i => i.OfficeAssignment)
+                .Include(i => i.Courses.Select(c => c.Department))
+                .OrderBy(i => i.LastName);
+
+            if(id != null)
+            {
+                ViewBag.InstructorID = id.Value;
+                viewModel.Courses = viewModel.Instructors.Where(i => i.InstructorID == id.Value).Single().Courses;
+            }
+            //if (courseID != null)
+            //{
+            //    ViewBag.CourseID = courseID.Value;
+            //    viewModel.Enrollments = viewModel.Courses.Where(x => x.CourseID == courseID).Single().Enrollments;
+            //}
+
+            //using Explicit Load
+            if (courseID != null)
+            {
+                ViewBag.CourseID = courseID.Value;
+                var selectedCourse = viewModel.Courses.Where(x => x.CourseID == courseID).Single();
+                db.Entry(selectedCourse).Collection(x => x.Enrollments).Load();
+                foreach (Enrollment enrollment in selectedCourse.Enrollments)
+                {
+                    db.Entry(enrollment).Reference(x => x.Student).Load();
+                }
+
+                viewModel.Enrollments = selectedCourse.Enrollments;
+            }
+                
+
+            return View(viewModel);
+        }
+
+        //
+        // GET: /Instructor/Details/5
+
+        public ViewResult Details(int id)
+        {
+            Instructor instructor = db.Instructors.Find(id);
+            return View(instructor);
+        }
+
+        //
+        // GET: /Instructor/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.InstructorID = new SelectList(db.OfficeAssignments, "InstructorID", "Location");
+            return View();
+        } 
+
+        //
+        // POST: /Instructor/Create
+
+        [HttpPost]
+        public ActionResult Create(Instructor instructor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Instructors.Add(instructor);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            ViewBag.InstructorID = new SelectList(db.OfficeAssignments, "InstructorID", "Location", instructor.InstructorID);
+            return View(instructor);
+        }
+        
+        //
+        // GET: /Instructor/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            Instructor instructor = db.Instructors.Find(id);
+            ViewBag.InstructorID = new SelectList(db.OfficeAssignments, "InstructorID", "Location", instructor.InstructorID);
+            return View(instructor);
+        }
+
+        //
+        // POST: /Instructor/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Instructor instructor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(instructor).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.InstructorID = new SelectList(db.OfficeAssignments, "InstructorID", "Location", instructor.InstructorID);
+            return View(instructor);
+        }
+
+        //
+        // GET: /Instructor/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            Instructor instructor = db.Instructors.Find(id);
+            return View(instructor);
+        }
+
+        //
+        // POST: /Instructor/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {            
+            Instructor instructor = db.Instructors.Find(id);
+            db.Instructors.Remove(instructor);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
